b46b0487f828d34a1009c6b664db049c6d6aebab
Vladimir Bykov <broadsw.rd@yandex.ru>
2017-07-21 12:22:17 +0300

    Fixed random

--- src/utils/random.js
+++ src/utils/random.js
@@ -1,15 +1,22 @@
+export function irand ({ min = 0, max = 1 } = {}) {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
 export function frand ({ min = 0, max = 1 } = {}) {
   min *= 100
   max *= 100
-  return Math.floor(Math.random() * (max - min) + min) / 100
-}
-
-export function irand ({ min = 0, max = 1 } = {}) {
-  return Math.floor(frand({ min, max }))
+  return irand({ min, max }) / 100
 }
 
 export function pick (array = []) {
-  return array[irand({ max: array.length })]
+  return array[irand({ max: array.length - 1 })]
+}
+
+export function take (array) {
+  var idx = irand({ max: array.length - 1 })
+  var item = array[idx]
+  array.splice(idx, 1)
+  return item
 }
 
 export function spread (value, spread) {
@@ -17,6 +24,6 @@ export function spread (value, spread) {
   return frand({ min: value - spread, max: value + spread })
 }
 
-export function chance (luck) {
-  return frand() < luck
+export function chance (likelihood) {
+  return Math.random() < likelihood
 }
