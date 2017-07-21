198260a3955267164ea993fb98cd71733cdb31b2
Vladimir Bykov <broadsw.rd@yandex.ru>
2017-07-21 13:09:35 +0300

    Refactor skills

--- src/skills/skill.js
+++ src/skills/skill.js
@@ -1,20 +1,15 @@
 class Skill {
-  static parse ({ name, value, description }) {
-    return new Skill({ name, value, description })
+  constructor ({ name, abilities }) {
+    this.name = name
+    this.abilities = abilities
   }
 
-  constructor ({ name, value, description }) {
-    this.name = name
-    this.value = value
-    this.description = description
+  apply (game) {
+    this.abilities.forEach(ability => ability.apply(game))
   }
 
   toJSON () {
-    return {
-      name: this.name,
-      value: this.value,
-      description: this.description
-    }
+    return this.abilities.map(ability => ability.toJSON())
   }
 }
 
